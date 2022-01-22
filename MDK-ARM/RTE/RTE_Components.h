64a1574540f93b10b9b1e81a487fb6fb72c2a37d
helloworld654 <zhai.shiqiang@163.com>
2022-01-22 14:32:57 +0800

    first commit

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'helloc8' 
+ * Target:  'helloc8' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ MDK-ARM/helloc8/startup_stm32f103xb.d
@@ -0,0 +1,1 @@
+helloc8\startup_stm32f103xb.o: startup_stm32f103xb.s
